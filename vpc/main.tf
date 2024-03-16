d8c3c883d72b2eedc085635a365fd456769abd55
ysakova90 <153944222+ysakova90@users.noreply.github.com>
2024-03-16 15:28:07 -0500

    changed vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -17,12 +17,12 @@ module "vpc" {
   }
 }
 output private_subnets {
-  description = "Prints out private_subnets
+  description = "Prints out private_subnets"
   value = module.vpc.private_subnets
 }
 
 
 outputs public_subnet {
-  description = " Prints out public_subnets
-  value = module.vpc. public_subnet
+  description = " Prints out public_subnets"
+  value = module.vpc. public_subnets
 }
