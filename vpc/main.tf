870056e228c0258e2fbe578ba290e2188fc4382d
ysakova90 <153944222+ysakova90@users.noreply.github.com>
2024-03-16 15:12:03 -0500

    added

--- vpc/main.tf
+++ vpc/main.tf
@@ -16,3 +16,13 @@ module "vpc" {
     Environment = "dev"
   }
 }
+output privet_subnets {
+  description = "Prints out private_subnets
+  value = module.vpc.private_subnets
+}
+
+
+outputs public_subnet {
+  description = " Prints out public_subnets
+  value = module.vpc. public_subnet
+}
