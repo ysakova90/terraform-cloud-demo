71423183efa3934e786e1298255ea4b6462d6750
ysakova90 <153944222+ysakova90@users.noreply.github.com>
2024-04-02 12:31:54 -0500

    changed

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,8 +1,8 @@
 output private_subnets {
-    descripton = "Prints out private_subnets"
+    description = "Prints out private_subnets"
     value = module.vpc.private_subnets
 }
 output public_subnets {
-    descripton = "Prints out public_subnets"
+    description = "Prints out public_subnets"
     value = module.vpc.public_subnets
 }
